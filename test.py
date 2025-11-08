eb4eb5e958b395a3616ce225aff368c7a9b87f02
w45242hy <hin.yip-2@manchester.ac.uk>
2025-11-08 12:48:57 +0000

    feat: add test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+from algo import extract_preferences
+
+mock_context = '{"message":"Jesse Bailey is a 50-year-old investor who started investing in 2016-05-17. His investment end date was 2018-02-15. His hobbies include learning languages, and he avoids energy and transportation, crypto assets, and life sciences. His true salary is $274484."}'
+
+if __name__ == "__main__":
+    input_message = eval(mock_context)["message"]
+
+    input_message = extract_preferences(input_message)
+
+    print(f"input_message: {input_message}")
+
+    print(f"Test completed ...")
+
